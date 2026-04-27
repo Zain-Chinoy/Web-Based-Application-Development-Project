3d4a718647683f094f08edad8d58905ccdb9a017
aafiafarooq <a.farooq.28595@khi.iba.edu.pk>
2026-04-28 02:31:38 +0500

    feat(backend): register interview cancellation route

--- routes/interviewRoutes.js
+++ routes/interviewRoutes.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { scheduleInterview, confirmInterview, getStudentInterviews, getRecruiterInterviews } = require('../controllers/interviewController');
+const { scheduleInterview, confirmInterview, cancelInterview, getStudentInterviews, getRecruiterInterviews } = require('../controllers/interviewController'); // Added cancelInterview
 
-router.route('/')
-    .post(scheduleInterview);
-
-router.route('/:id/confirm')
-    .put(confirmInterview);
-
-router.route('/student/:studentId')
-    .get(getStudentInterviews);
-
-router.route('/recruiter/:recruiterId')
-    .get(getRecruiterInterviews);
+router.route('/').post(scheduleInterview);
+router.route('/:id/confirm').put(confirmInterview);
+router.route('/:id/cancel').put(cancelInterview); // NEW ROUTE
+router.route('/student/:studentId').get(getStudentInterviews);
+router.route('/recruiter/:recruiterId').get(getRecruiterInterviews);
 
 module.exports = router;
